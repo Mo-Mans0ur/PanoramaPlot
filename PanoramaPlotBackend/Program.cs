b4581671301816f565f1eb5bb37570eb5cce4562
Nicolas-Mousten <nicolas.mousten@gmail.com>
2024-06-08 14:16:14 +0200

    Added search api

--- PanoramaPlotBackend/Program.cs
+++ PanoramaPlotBackend/Program.cs
@@ -209,6 +209,85 @@ app.MapGet("/movies/{page:int?}", async context =>
     }
 });
 
+app.MapPost("movies/search/{query}/{page:int?}", async context => 
+{
+    string RequestPage = context.Request.RouteValues["page"]?.ToString() ?? "1";
+    string RequestQuery = context.Request.RouteValues["query"]?.ToString();
+    string api_key = Environment.GetEnvironmentVariable("TMDB_KEY");
+    string api_token = Environment.GetEnvironmentVariable("TMDB_READ_ACCESS_TOKEN");
+    
+    RequestQuery = Uri.EscapeDataString(RequestQuery); //Needed to format the string to HexEscape for IMDB api
+    
+    if( string.IsNullOrEmpty(RequestQuery) ){
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync("Query parameter is required.");
+    }
+    string apiURL = $"https://api.themoviedb.org/3/search/movie?query={RequestQuery}&include_adult=false&language=en-US&page={RequestPage}";
+    
+    using (HttpClient client = new HttpClient())
+    {
+        client.DefaultRequestHeaders.Add("accept", "application/json");
+        client.DefaultRequestHeaders.Add("Authorization", "Bearer " + api_token);
+        HttpResponseMessage response = await client.GetAsync(apiURL);
+        Console.WriteLine("Response: " + response);
+        if (response.IsSuccessStatusCode)
+        {
+            // Read the JSON response as a string
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+
+            // Deserialize JSON string into dynamic object using Newtonsoft.Json
+            dynamic jsonObject = JsonConvert.DeserializeObject(jsonResponse);
+
+            JArray resultsArray = jsonObject.results;
+
+            List<Movie> movies = new List<Movie>();
+
+            foreach (JToken result in resultsArray)
+            {
+                Movie movie = new Movie(
+                    result["adult"]?.ToString(), 
+                    result["backdrop_path"]?.ToString(), 
+                    result["genre_ids"]?.ToString(), 
+                    result["id"]?.ToString(),
+                    result["original_language"]?.ToString(), 
+                    result["original_title"]?.ToString(),
+                    result["overview"]?.ToString(), 
+                    result["popularity"]?.ToString(), 
+                    result["poster_path"]?.ToString(), 
+                    result["release_date"]?.ToString(),
+                    result["title"]?.ToString(), 
+                    result["vote_average"]?.ToString(), 
+                    result["vote_count"]?.ToString()
+                );
+
+                movies.Add(movie);
+            }
+            //------------------------------------------Above to get the data and manipulate it. Under to create the url for getting new data
+            string UrlNext = "";
+            string UrlPrevious = "";
+            
+            if(int.Parse(RequestPage) >= 1 && int.Parse(RequestPage) < 500){
+                UrlNext = Environment.GetEnvironmentVariable("URL")+$"movies/{int.Parse(RequestPage)+1}";
+            }
+            if(int.Parse(RequestPage) <= 500 && int.Parse(RequestPage) > 1){
+                UrlPrevious = Environment.GetEnvironmentVariable("URL")+$"movies/{int.Parse(RequestPage)-1}";
+            }
+            
+
+
+            string jsonSerialized = JsonConvert.SerializeObject(new { data = movies, url_path = new { previous = UrlPrevious, next = UrlNext }});
+
+            context.Response.Headers.Add("Content-Type", "application/json");
+            await context.Response.WriteAsync(jsonSerialized);
+        }
+        else
+        {
+            // Write error message to the response stream
+            await context.Response.WriteAsync("Failed to fetch data.");
+        }
+    }
+
+});
 
 app.Run();
 
